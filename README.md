958213ae31f5672e3fbbea10d711e5095c6eeb03
Javier Martinez <hoogmin@gmail.com>
2023-05-14 17:35:13 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+An emacs config. It is quite simple. Clone this repo to a device home dir
+and rename it to the standard config directory name (usually `.emacs.d`).
---
+++ init.el
@@ -0,0 +1,27 @@
+;; Disable start screen, get back to it anytime with
+;; C-h C-a
+(setq inhibit-startup-message t)
+
+;; Disable tool bar, menu bar, scroll bar.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Enable line numbers
+(line-number-mode t)
+
+;; Disable automatic ~file backups.
+(setq make-backup-files nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(wombat)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
